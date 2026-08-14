360af08e872c22d760b837592687a2e8a6e76363
Cassandra <cassiedejucos.03@gmail.com>
2026-08-14 22:35:48 +1000

    both python and jupyter files

---
+++ EvenSum.py
@@ -0,0 +1,12 @@
+# Student ID: K240945
+# Name: Cassandra Noeribelle Dejucos
+
+num = list(range(101))
+
+even_sum = 0
+
+for num in num:
+    if num % 2 == 0:
+        even_sum += num
+
+print(f'Sum of even numbers from 1 to 100:', even_sum)
